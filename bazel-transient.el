d327aa880dc71a2e53894d88c776431d400d5ab5
Jonathan Jin <jjin082693@gmail.com>
2020-07-19 13:37:30 -0400

    Remove bazel-transient-test for now
    Most functionality in there is "core" functionality not tied to testing.

--- bazel-transient.el
+++ bazel-transient.el
@@ -35,8 +35,6 @@
 (require 's)
 (require 'dash)
 
-(require 'bazel-transient-test)
-
 (defcustom bazel-transient-bazel-cmd
   "bazel"
   "Command to run Bazel with."
@@ -50,6 +48,18 @@
   :options '(default
               ivy)
   :type 'symbol)
+
+(defcustom bazel-transient-enable-caching
+  nil
+  "Whether or not to enable caching for Bazel operations."
+  :group 'bazel-transient
+  :type 'bool)
+
+(defcustom bazel-transient-cache-file
+  (expand-file-name "bazel-transient-targets.cache" user-emacs-directory)
+  "The name of the Bazel-Transient test target cache file."
+  :group 'bazel-transient
+  :type 'string)
 
 ;; FIXME: Copied wholesale from magit-utils.el. Upstream a PR to
 ;; transient that decouples this from magit.
@@ -212,6 +222,20 @@ ARGS is forwarded to Bazel as test command flags."
     ("t" "target" bazel-test-target)
     ("c" "all in current pkg" bazel-transient-test-all-in-current-package)
     ]])
+
+(defvar bazel-transient-kind-target-cache
+  (make-hash-table :test 'equal)
+  "A hashmap used to cache project targets by kind.")
+
+;; Credit to bbatsov's projectile-serialize
+(defun bazel-transient-serialize (data filename)
+  (when (file-writable-p filename)
+    (with-temp-file filename
+      (insert (let (print-legth) (prin1-to-string data))))))
+
+(defun bazel-transient-serialize-kind-target-cache ()
+  "Serializes the test cache to the hard drive."
+  (message "TODO: bazel-transient-serialize-kind-target-cache"))
 
 (provide 'bazel-transient)
 
